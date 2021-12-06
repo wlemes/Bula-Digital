bbd1f392a183d90e37426d79b6f62ea617f2d748
wendellemes0 <wendellemes0@gmail.com>
2021-12-05 20:15:30 -0400

    atualizações

--- BulaDigital.WEB/Program.cs
+++ BulaDigital.WEB/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Bula}/{action=Index}/{id?}");
 
 app.Run();
